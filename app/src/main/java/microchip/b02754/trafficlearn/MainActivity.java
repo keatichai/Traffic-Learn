790e313255f0c04cb1c7f2f7a2bea73dfb74308b
Keatichai <Keatichaisucharit@gmail.com>
2016-09-11 10:45:12 +0700

    Webview Success full

--- app/src/main/java/microchip/b02754/trafficlearn/MainActivity.java
+++ app/src/main/java/microchip/b02754/trafficlearn/MainActivity.java
@@ -1,13 +1,53 @@
 package microchip.b02754.trafficlearn;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
+    //Explicit declare variable
+    private ListView listView;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        // Bind Widget
+        listView = (ListView) findViewById(R.id.listView);
+        button = (Button) findViewById(R.id.button);
+
+        //Button Controller
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Sound Effect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.phonton1);
+                mediaPlayer.start();
+
+                //Show web view
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://youtu.be/bpeBugHSCnU"));
+                startActivity(intent);
+
+
+
+                //mediaPlayer.stop();
+
+
+            }//onClick
+        });
+
+
+
+
+    }// Main Method
+}       //Main class
